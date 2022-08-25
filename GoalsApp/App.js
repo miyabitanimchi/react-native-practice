316b3f3af5b5590d538ae1cc700c4c6354ee2227
miyabitanimchi <mmyybb.mm@gmail.com>
2022-08-24 18:53:51 -0700

    make some components

--- GoalsApp/App.js
+++ GoalsApp/App.js
@@ -1,23 +1,16 @@
 import { useState } from 'react';
 import {
   StyleSheet,
-  Text,
-  TextInput,
   View,
-  Button,
-  ScrollView,
   FlatList
 } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoalText(enteredText);
-  }
-
-  const addGoalHandler = () => {
+  const addGoalHandler = (enteredGoalText) => {
     setCourseGoals((prevState) => [
       ...prevState,
       // this key will be automatically used in Flatlist
@@ -28,22 +21,13 @@ export default function App() {
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-        style={styles.textInput}
-        placeholder="Your course goal!"
-        onChangeText={goalInputHandler}
+      <GoalInput
+        onAddGoal={addGoalHandler}
       />
-        <Button title="Add Goal" onPress={addGoalHandler} />
-      </View>
       <View style={styles.goalsContainer}>
         <FlatList
           data={courseGoals}
-          renderItem={(itemData) => (
-            <View style={styles.goalItem}>
-              <Text style={styles.goalText}>{itemData.item.text}</Text>
-            </View>
-          )}
+          renderItem={(itemData) => <GoalItem text={itemData.item.text} />}
           alwaysBounceVertical={false}
           keyExtractor={(item, index) => {
             return item.id
@@ -61,33 +45,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     flex: 1
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc"
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 8,
-    padding: 8
-  },
   goalsContainer: {
     flex: 5
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "salmon",
-
-  },
-  goalText: {
-    color: "white"
   }
 });
---
+++ GoalsApp/components/GoalItem.js
@@ -0,0 +1,24 @@
+import { StyleSheet, View, Text } from "react-native";
+
+const GoalItem = ({ text }) => {
+  return (
+    <View style={styles.goalItem}>
+      <Text style={styles.goalText}>{text}</Text>
+    </View>
+  )
+}
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "salmon",
+
+  },
+  goalText: {
+    color: "white"
+  }
+});
