48e9429b9a06a4110d3cc3bc67e301bf76b5d9f0
miyabitanimchi <mmyybb.mm@gmail.com>
2022-09-03 16:24:27 -0700

    54 add root bg color

--- GuessMyNumber/App.js
+++ GuessMyNumber/App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+    return (
+        <View style={styles.rootScreen}>
+            <StartGameScreen />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-
+    rootScreen: {
+        backgroundColor: "#edf7f6",
+        flex: 1,
+    },
 });
