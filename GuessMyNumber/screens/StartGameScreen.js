1427c3b9fe633d4e49310715f8a0e41833643121
miyabitanimchi <mmyybb.mm@gmail.com>
2022-09-03 14:47:41 -0700

    add style on button container and add shadow

--- GuessMyNumber/screens/StartGameScreen.js
+++ GuessMyNumber/screens/StartGameScreen.js
@@ -1,9 +1,9 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet} from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
     return (
-        <View>
+        <View style={styles.inputContainer}>
             <TextInput />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
@@ -12,3 +12,18 @@ const StartGameScreen = () => {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        backgroundColor: "#fea49f",
+        borderRadius: 8,
+        elevation: 5, // android only
+        shadowColor: "#393f4d",
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    }
+})
