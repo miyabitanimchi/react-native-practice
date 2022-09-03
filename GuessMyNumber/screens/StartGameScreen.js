1985d21cf3de5bf4ea08a63f5f27525828e58250
miyabitanimchi <mmyybb.mm@gmail.com>
2022-09-03 14:58:21 -0700

    style number input

--- GuessMyNumber/screens/StartGameScreen.js
+++ GuessMyNumber/screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import PrimaryButton from "../components/PrimaryButton";
 const StartGameScreen = () => {
     return (
         <View style={styles.inputContainer}>
-            <TextInput />
+            <TextInput style={styles.numberInput} maxLength={2} />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
@@ -25,5 +25,16 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 6,
         shadowOpacity: 0.25
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: "#101357",
+        borderBottomWidth: 2,
+        color: "#101357",
+        marginVertical: 8,
+        fontWeight: "bold",
+        textAlign: "center"
     }
 })
