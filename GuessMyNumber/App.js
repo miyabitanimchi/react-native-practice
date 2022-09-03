ce16eebcb92dee48946df52215cc7b5f9220ccc8
miyabitanimchi <mmyybb.mm@gmail.com>
2022-09-03 16:48:27 -0700

    56 add bg image

--- GuessMyNumber/App.js
+++ GuessMyNumber/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 
@@ -9,14 +9,23 @@ export default function App() {
             colors={["#D66D75", "#E29587"]}
             style={styles.rootScreen}
         >
-            <StartGameScreen />
+            <ImageBackground
+                source={require("./assets/images/background.png")}
+                resizeMode="cover"
+                style={styles.rootScreen}
+                imageStyle={styles.backgroundImage}
+            >
+                <StartGameScreen />
+            </ImageBackground>
         </LinearGradient>
     );
 }
 
 const styles = StyleSheet.create({
     rootScreen: {
-        backgroundColor: "#edf7f6",
         flex: 1,
+    },
+    backgroundImage: {
+        opacity: 0.1,
     },
 });
