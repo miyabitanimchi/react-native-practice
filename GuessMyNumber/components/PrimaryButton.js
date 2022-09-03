6d8bb3f4d35326801d7ff98a928fcfbb9b0225f4
miyabitanimchi <mmyybb.mm@gmail.com>
2022-09-03 15:32:02 -0700

    52 add style and effect on custom btns

--- GuessMyNumber/components/PrimaryButton.js
+++ GuessMyNumber/components/PrimaryButton.js
@@ -1,11 +1,41 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 const PrimaryButton = ({ children }) => {
     return (
-        <View>
-            <Text>{children}</Text>
+        <View style={styles.buttonOuterContainer}>
+            <Pressable
+                style={({ pressed }) =>
+                    pressed
+                        ? [styles.buttonInnerContainer, styles.pressed]
+                        : styles.buttonInnerContainer
+                }
+                android_ripple={{ color: "#10138a" }}
+            >
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
         </View>
-    )
-}
+    );
+};
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: "hidden",
+    },
+    buttonInnerContainer: {
+        backgroundColor: "#101357",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 4,
+    },
+    buttonText: {
+        color: "#edf7f6",
+        textAlign: "center",
+    },
+    pressed: {
+        opacity: 0.75,
+    },
+});
