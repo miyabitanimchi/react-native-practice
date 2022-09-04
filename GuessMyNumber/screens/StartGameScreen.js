c480a98692698495a9d74874bd29222995a5e605
miyabitanimchi <mmyybb.mm@gmail.com>
2022-09-03 17:17:57 -0700

    58 game logic - add alert

--- GuessMyNumber/screens/StartGameScreen.js
+++ GuessMyNumber/screens/StartGameScreen.js
@@ -1,4 +1,4 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import { useState } from "react";
 
@@ -9,8 +9,27 @@ const StartGameScreen = () => {
         setEnteredNumber(enteredText);
     };
 
+    const resetInputHandler = () => {
+        setEnteredNumber("");
+    };
+
     const confirmInputHandler = () => {
-        console.log("pressed");
+        const chosenNumber = parseInt(enteredNumber);
+
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            Alert.alert(
+                "Invalid Number",
+                "Number has to be a number between 1 and 99",
+                [
+                    {
+                        text: "Okay",
+                        style: "destructive",
+                        onPress: resetInputHandler,
+                    },
+                ]
+            );
+            return;
+        }
     };
     return (
         <View style={styles.inputContainer}>
@@ -25,7 +44,9 @@ const StartGameScreen = () => {
             />
             <View style={styles.buttonsContainer}>
                 <View style={styles.buttonContainer}>
-                    <PrimaryButton>Reset</PrimaryButton>
+                    <PrimaryButton onPress={resetInputHandler}>
+                        Reset
+                    </PrimaryButton>
                 </View>
                 <View style={styles.buttonContainer}>
                     <PrimaryButton onPress={confirmInputHandler}>
