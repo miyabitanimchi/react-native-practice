e06a8c71a0e256b50e0382543cfc8f23b0182d92
miyabitanimchi <mmyybb.mm@gmail.com>
2022-09-07 23:35:47 -0700

    72. add image

--- GuessMyNumber/App.js
+++ GuessMyNumber/App.js
@@ -38,7 +38,7 @@ export default function App() {
         );
     }
 
-    if (gameIsOver && userNumber) {
+    if (!gameIsOver && !userNumber) {
         screen = <GameOverScreen />;
     }
 
--- GuessMyNumber/screens/GameOverScreen.js
+++ GuessMyNumber/screens/GameOverScreen.js
@@ -1,7 +1,39 @@
-import { Text } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 
 const GameOverScreen = () => {
-    return <Text>this is a game over screen</Text>;
+    return (
+        <View style={styles.rooteContainer}>
+            <Title>GAME OVER</Title>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={require("../assets/images/success.png")} />
+            </View>
+            <Text>Your phone needed X rounds to guess the number Y.</Text>
+        </View>
+    )
 };
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+    rooteContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 24
+    },
+    imageContainer: {
+        borderRadius: 150,
+        width: 300,
+        height: 300,
+        borderWidth: 3,
+        borderColor: Colors.accent2_500,
+        overflow: "hidden",
+        margin: 36
+    },
+    image: {
+        width: "100%",
+        height: "100%"
+    }
+})
