fbdc7da60429bf0d2ead1bd1173054623521c295
miyabitanimchi <mmyybb.mm@gmail.com>
2022-09-21 16:39:06 -0700

    95. implement navigation btwn two screens

--- MealsApp/App.js
+++ MealsApp/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -13,6 +14,10 @@ export default function App() {
                     <Stack.Screen
                         name="MealsCategories"
                         component={CategoriesScreen}
+                    />
+                    <Stack.Screen
+                        name="MealsOverview"
+                        component={MealsOverviewScreen}
                     />
                 </Stack.Navigator>
             </NavigationContainer>
--- MealsApp/screens/CategoriesScreen.js
+++ MealsApp/screens/CategoriesScreen.js
@@ -2,16 +2,20 @@ import { CATEGORIES } from "../data/dummy-data";
 import { FlatList } from "react-native";
 import CategoryGridTile from "../components/CategoryGridTile";
 
-const renderCategoryItem = (itemData) => {
-    return (
-        <CategoryGridTile
-            title={itemData.item.title}
-            color={itemData.item.color}
-        />
-    );
-};
+const CategoriesScreen = ({ navigation }) => {
+    const renderCategoryItem = (itemData) => {
+        const pressHandler = () => {
+            navigation.navigate("MealsOverview");
+        };
+        return (
+            <CategoryGridTile
+                title={itemData.item.title}
+                color={itemData.item.color}
+                onPress={pressHandler}
+            />
+        );
+    };
 
-const CategoriesScreen = () => {
     return (
         <FlatList
             data={CATEGORIES}
