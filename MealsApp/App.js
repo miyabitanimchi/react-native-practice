17934a8a2ec3f46cda13d6fc17f295b478bb4cb3
miyabitanimchi <mmyybb.mm@gmail.com>
2022-09-25 23:20:12 -0700

    112. add style and icons on drawer

--- MealsApp/App.js
+++ MealsApp/App.js
@@ -7,6 +7,8 @@ import FavoritesScreen from "./screens/FavoritesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Ionicons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -20,14 +22,32 @@ const DwawerNavigator = () => {
                 },
                 headerTintColor: "black",
                 sceneContainerStyle: { backgroundColor: "#f2f2f2" },
+                drawerContentStyle: { backgroundColor: "#9eecff" },
+                drawerInactiveTintColor: "gray",
+                drawerActiveTintColor: "#0580FA",
+                drawerActiveBackgroundColor: "#DCECFC",
             }}
         >
             <Drawer.Screen
                 name="Categories"
                 component={CategoriesScreen}
-                options={{ title: "All Categories" }}
+                options={{
+                    title: "All Categories",
+                    drawerIcon: ({ color, size }) => (
+                        <Ionicons name="list" size={size} color={color} />
+                    ),
+                }}
             />
-            <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+            <Drawer.Screen
+                name="Favorites"
+                component={FavoritesScreen}
+                options={{
+                    title: "All Categories",
+                    drawerIcon: ({ color, size }) => (
+                        <AntDesign name="heart" size={size} color={color} />
+                    ),
+                }}
+            />
         </Drawer.Navigator>
     );
 };
