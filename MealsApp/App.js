9ba27ff4f0fef34359285d90bad0146d1235e379
miyabitanimchi <mmyybb.mm@gmail.com>
2022-09-25 23:07:48 -0700

    112. add drawer

--- MealsApp/App.js
+++ MealsApp/App.js
@@ -3,10 +3,35 @@ import { StyleSheet, Button } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DwawerNavigator = () => {
+    return (
+        <Drawer.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#9eecff",
+                },
+                headerTintColor: "black",
+                sceneContainerStyle: { backgroundColor: "#f2f2f2" },
+            }}
+        >
+            <Drawer.Screen
+                name="Categories"
+                component={CategoriesScreen}
+                options={{ title: "All Categories" }}
+            />
+            <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+        </Drawer.Navigator>
+    );
+};
+
 export default function App() {
     return (
         <>
@@ -21,15 +46,11 @@ export default function App() {
                 // }}
                 >
                     <Stack.Screen
-                        name="MealsCategories"
-                        component={CategoriesScreen}
+                        name="Drawer"
+                        component={DwawerNavigator}
                         options={{
-                            title: "All Categories",
-                            headerStyle: {
-                                backgroundColor: "#9eecff",
-                            },
-                            headerTintColor: "black",
-                            contentStyle: { backgroundColor: "#f2f2f2" },
+                            headerShown: false,
+                            // title: "All Categories",
                         }}
                     />
                     <Stack.Screen
---
+++ MealsApp/screens/FavoritesScreen.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const FavoritesScreen = () => {
+    return <Text>The favorite components</Text>;
+};
+
+export default FavoritesScreen;
