8445b2ee1b2afd06ebfe5baca52d19c9b656ec41
miyabitanimchi <mmyybb.mm@gmail.com>
2022-09-27 23:27:41 -0700

    122 display data on state

--- MealsApp/screens/FavoritesScreen.js
+++ MealsApp/screens/FavoritesScreen.js
@@ -3,12 +3,15 @@ import MealsList from "../components/MealsList/MealsList";
 import { useContext } from "react";
 import { FavoriteContext } from "../store/context/favorite-context";
 import { MEALS } from "../data/dummy-data";
+import { useSelector } from "react-redux";
 
 const FavoritesScreen = () => {
     const favoriteMealsCtx = useContext(FavoriteContext);
+    const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
 
     const favoriteMeals = MEALS.filter((meal) =>
-        favoriteMealsCtx.ids.includes(meal.id)
+    favoriteMealIds.includes(meal.id)
+        // favoriteMealsCtx.ids.includes(meal.id)
     );
 
     if (favoriteMeals.length === 0) {
