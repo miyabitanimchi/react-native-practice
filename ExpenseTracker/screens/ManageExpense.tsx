d40a5b21a12b8b6f1ee45e50609e4ccff23e1ee3
miyabitanimchi <mmyybb.mm@gmail.com>
2022-09-28 00:55:01 -0700

    setup some screens and install react navigators

---
+++ ExpenseTracker/screens/ManageExpense.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const ManageExpenses = () => {
+  return (
+    <View>
+      <Text>Manage Expense Screen</Text>
+    </View>
+  );
+};
+
+export default ManageExpenses;
---
+++ ExpenseTracker/screens/RecentExpenses.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const RecentExpenses = () => {
+  return (
+    <View>
+      <Text>Recent Expenses Screen</Text>
+    </View>
+  );
+};
+
+export default RecentExpenses;
