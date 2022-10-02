ebffa4b4691cb27aa5def1a36ba03f84bdc0ed45
miyabitanimchi <mmyybb.mm@gmail.com>
2022-10-02 13:30:03 -0700

    148. create and configure form

--- ExpenseTracker2/components/ManageExpense/ExpenseForm.js
+++ ExpenseTracker2/components/ManageExpense/ExpenseForm.js
@@ -1,7 +1,28 @@
 import { View } from 'react-native';
+import Input from './Input';
 
 const ExpenseForm = () => {
-  return <View></View>;
+  const amountChangeHandler = () => {};
+  return (
+    <View>
+      <Input
+        label="Amount"
+        textInputConfig={{
+          keyboardType: 'decimal-pad',
+          onChangeText: amountChangeHandler,
+        }}
+      />
+      <Input
+        label="Date"
+        textInputConfig={{
+          placeholder: 'YYYY-MM-DD',
+          maxLength: 10,
+          onChangeText: amountChangeHandler,
+        }}
+      />
+      <Input label="Description" textInputConfig={{}} />
+    </View>
+  );
 };
 
 export default ExpenseForm;
