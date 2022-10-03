db6e3189c1a5d5087492d202f614cdbc03f2ff86
miyabitanimchi <mmyybb.mm@gmail.com>
2022-10-02 18:49:26 -0700

    155. add validation

--- ExpenseTracker2/components/ManageExpense/ExpenseForm.js
+++ ExpenseTracker2/components/ManageExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Alert } from 'react-native';
 import Input from './Input';
 import { useState } from 'react';
 import Button from '../UI/Button';
@@ -28,6 +28,15 @@ const ExpenseForm = ({
       date: new Date(inputValues.date),
       description: inputValues.description,
     };
+
+    const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+    const dateIsValid = expenseData.date.toString() !== 'Invalid Date';
+    const descriptionIsValid = expenseData.description.trim().length > 0;
+
+    if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+      Alert.alert('Invalid input', 'Please check your input values');
+      return;
+    }
 
     onSubmit(expenseData);
   };
