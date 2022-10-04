eb5f73b43aae348a9529b1259bb5b5de71b0b8c9
miyabitanimchi <mmyybb.mm@gmail.com>
2022-10-04 13:43:50 -0700

    184. add function to protect resources

--- Authentication/screens/WelcomeScreen.js
+++ Authentication/screens/WelcomeScreen.js
@@ -1,11 +1,28 @@
 import * as React from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [ferchedMessage, setFetchedMessage] = useState('');
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+  useEffect(() => {
+    axios
+      .get(
+        `https://react-native-course-3b9f6-default-rtdb.firebaseio.com/message.json?auth=${token}`
+      )
+      .then((res) => {
+        setFetchedMessage(res.data);
+      });
+  }, []);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{ferchedMessage}</Text>
     </View>
   );
 }
